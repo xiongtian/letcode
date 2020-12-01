4fe193b54b1f90f84ce2d1d9470954e101d164f8
xiongtian <764578422@qq.com>
2020-12-01 12:50:41 +0800

    在排序数组中查找元素的第一个和最后一个位置

--- src/main/java/com/letcode/y2020/m12/Question01.java
+++ src/main/java/com/letcode/y2020/m12/Question01.java
@@ -50,16 +50,16 @@ public class Question01 {
     }
 
     public int binarySearch(int[] nums,int target,boolean lower) {
-        int left = 0,right = nums.length-1,ans = nums.length;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] > target || (lower && nums[mid] >= target)) {
-                right = mid - 1;
-                ans= mid;
-            }else {
-                left = mid+1;
-            }
-        }
+       int left = 0, right = nums.length-1,ans=nums.length;
+       while (left<=right){
+           int mid = (left+right)/2;
+           if (nums[mid] > target || (lower && nums[mid]>=target)){
+               right = mid - 1;
+               ans=mid;
+           }else {
+               left = mid+1;
+           }
+       }
         return ans;
     }
 
