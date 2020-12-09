0bd306411c0573fb22f2c3f9862be0d95255e8a9
xiongtian <764578422@qq.com>
2020-12-09 19:08:44 +0800

    节点进行排序

---
+++ src/main/java/com/letcode/y2020/m12/Question02.java
@@ -0,0 +1,66 @@
+package com.letcode.y2020.m12;
+
+/**
+ * @Author xiongtian
+ * @Date 2020/12/8 9:29
+ * @Version 1.0
+ *
+ * 题目：
+ * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+ *
+ * 进阶：
+ *
+ *     你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+ *
+ */
+
+public class Question02 {
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        ListNode sorted = merge(list1, list2);
+        return sorted;
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+
+}
--- src/main/java/com/letcode/y2020/m12/Question03.java
+++ src/main/java/com/letcode/y2020/m12/Question03.java
@@ -47,12 +47,11 @@ public class Question03 {
         } else if (l2 == null) {
             return l1;
         } else if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
+            l1.next = mergeTwoLists(l1.next,l2);
             return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
+        }else {
+            l2.next = mergeTwoLists(l1,l2.next);
             return l2;
         }
-
     }
 }
