79dd3b5bc8027641a045de383781ec2dd3c39a73
xiongtian <764578422@qq.com>
2020-12-11 10:40:00 +0800

    链表的排序以及递归算法的学习

---
+++ src/main/java/com/letcode/y2020/m12/Question04.java
@@ -0,0 +1,64 @@
+package com.letcode.y2020.m12;
+
+import java.util.List;
+
+/**
+ * @Author xiongtian
+ * @Date 2020/12/10 9:14
+ * @Version 1.0
+ * <p>
+ * 将所有的数字进行组合
+ */
+
+public class Question04 {
+
+    public static void main(String[] args) {
+        Question04 test = new Question04();
+        String A = "abc";
+        char[] arrayA = A.toCharArray();
+        test.recursionArrange(arrayA,0,arrayA.length-1);
+    }
+    /*
+     * 参数arrayA:给定字符串的字符数组
+     * 参数start:开始遍历字符与其后面各个字符将要进行交换的位置
+     * 参数end:字符串数组的最后一位
+     * 函数功能：输出字符串数字的各个字符全排列
+     */
+    public void recursionArrange(char[] arrayA,int start,int end){
+        if(end <= 1)
+            return;
+        if(start == end){
+            for(int i = 0;i < arrayA.length;i++)
+                System.out.print(arrayA[i]);
+            System.out.println();
+        }
+        else{
+            for(int i = start;i <= end;i++){
+                swap(arrayA,i,start);
+                recursionArrange(arrayA,start+1,end);
+                swap(arrayA,i,start); // 还原字符串，为一下一轮for循环做准备
+            }
+        }
+
+    }
+    //交换数组m位置和n位置上的值
+    public void swap(char[] arrayA,int m,int n){
+        char temp = arrayA[m];
+        arrayA[m] = arrayA[n];
+        arrayA[n] = temp;
+    }
+
+    public void ComposeAllNum(String src) {
+        char[] chars = src.toCharArray();
+        for (int i = 0; i < src.length(); i++) {
+            String resu = String.valueOf(src.charAt(i));
+            for (int j = 0; j < src.length(); j++) {
+                if (i != j) {
+                     resu = resu+String.valueOf(src.charAt(j));
+                    System.out.println(resu);
+                }
+
+            }
+        }
+    }
+}
---
+++ src/test/m12/TestRecursive.java
@@ -0,0 +1,83 @@
+package m12;
+
+/**
+ * @Author xiongtian
+ * @Date 2020/12/11 9:18
+ * @Version 1.0
+ * <p>
+ * <p>
+ * 递归练习
+ */
+
+public class TestRecursive {
+    public static void main(String[] args) {
+        TestRecursive t = new TestRecursive();
+
+        // 递归
+        System.out.println(t.factorial(5));
+
+        // 斐波那契数列
+        System.out.println(t.fib(5));
+        System.out.println(t.fib1(5));
+    }
+
+    /**
+     * 阶乘
+     */
+    public int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(--n);
+    }
+
+    /**
+     * 0 1 1 2 3 5 8 13 21 34
+     * 斐波那契数列 fibonacci
+     * 递归实现
+     */
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    /**
+     * 斐波那契数列的其他实现
+     * 提交git的时候使用普通递归的方式，会超时编译不通过
+     * 这里使用数组的方式进行存储优化
+     */
+    public int fib1(int n) {
+        int[] flag = new int[n + 1];
+        return dp(n, flag);
+    }
+
+    private int dp(int n, int[] flag) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        flag[n] = flag[n] == 0 ? (dp(n - 1, flag) + dp(n - 2, flag)) : flag[n];
+        return flag[n];
+    }
+
+    /**
+     * 为了符合letcode的时间复杂度，另一种使用动态规划的解题方法
+     * @param n
+     * @return
+     */
+    public int fib2(int n){
+        if(n == 0) return 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] %= 1000000007;
+        }
+        return dp[n];
+    }
+
+}
